ff5acf41645461fb100a626a2a3ed049db5139c0
SabrinaGiancaspro <sabrina.giancaspro13@gmail.com>
2023-10-12 16:34:24 +0200

    add js src & readme

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// #Palindroma
+// Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
+// -chiedere all'utente di inserire la parola tramite prompt 
+// -dichiarare una variabile per la parola inserita 
+// -creare funzione con un parametro (word)
+//   -fare l'operazione della funzione e invertiamo i caratteri (reverse())
+// -confrontiamo le due parole (word === (word.reverse))
+// -SE la parola è uguale è true
+//   -ALTRIMENTI è false
