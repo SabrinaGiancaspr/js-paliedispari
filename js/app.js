168fd00bb03d998a2cb4a8b46d944ce5b6c19ec2
SabrinaGiancaspro <sabrina.giancaspro13@gmail.com>
2023-10-12 18:20:04 +0200

    add console log

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 
 // -chiedere all'utente di inserire la parola tramite prompt 
 let word = prompt ('inserisci una parola');//-dichiarare una variabile per la parola inserita 
-
+console.log(word)
 let reverse = '';       //-dichiarare variabile per parola al contrario
 
 for (let i = word.length -1; i >= 0; i--){//-ciclo for per scomporre le lettere della parola e decrementando stamparla al contrario
